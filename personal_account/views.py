c65348277c34455746c128f0b006ee1d80e2f8db
Cookie <Cookie@Cookie.local>
2017-05-16 16:48:38 +0300

    Refactor home page view to use a template

--- personal_account/views.py
+++ personal_account/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse('<html><title>Personal Finance</title></html>')
+    return render(request, 'home.html')
