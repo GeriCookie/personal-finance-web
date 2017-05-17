923dfc0d3357f601f4b3bfcdeecab2a321fe40a5
Cookie <Cookie@Cookie.local>
2017-05-18 01:20:03 +0300

    Models for Income and Expenses and assosiated migrations

--- personal_account/models.py
+++ personal_account/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from decimal import Decimal
 
-# Create your models here.
+
+class Income(models.Model):
+    category = models.TextField(default='')
+    amount = models.DecimalField(
+            max_digits=19,
+            decimal_places=2,
+            default=Decimal('0.00')
+            )
+
+
+class Expense(models.Model):
+    category = models.TextField(default='')
+    amount = models.DecimalField(
+            max_digits=19,
+            decimal_places=2,
+            default=Decimal('0.00')
+            )
--- personal_account/tests.py
+++ personal_account/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from personal_account.models import Income, Expense
 
 
 class HomePageTest(TestCase):
@@ -14,3 +15,48 @@ class HomePageTest(TestCase):
                 })
         self.assertIn('Salary: 1000', response_income.content.decode())
         self.assertTemplateUsed(response_income, 'home.html')
+
+
+class IncomeModelTest(TestCase):
+
+    def test_saving_and_retrieving_incomes(self):
+        first_income = Income()
+        first_income.category = 'Salary'
+        first_income.amount = 1000
+        first_income.save()
+
+        second_income = Income()
+        second_income.category = 'Bonus'
+        second_income.amount = 2000
+        second_income.save()
+
+        saved_incomes = Income.objects.all()
+        self.assertEqual(saved_incomes.count(), 2)
+
+        first_saved_income = saved_incomes[0]
+        second_saved_income = saved_incomes[1]
+        self.assertEqual(first_saved_income.category, 'Salary')
+        self.assertEqual(first_saved_income.amount, 1000)
+        self.assertEqual(second_saved_income.category, 'Bonus')
+        self.assertEqual(second_saved_income.amount, 2000)
+
+    def test_saving_and_retrieving_expenses(self):
+        first_expense = Expense()
+        first_expense.category = 'Salary'
+        first_expense.amount = 1000
+        first_expense.save()
+
+        second_expense = Expense()
+        second_expense.category = 'Bonus'
+        second_expense.amount = 2000
+        second_expense.save()
+
+        saved_expenses = Expense.objects.all()
+        self.assertEqual(saved_expenses.count(), 2)
+
+        first_saved_expense = saved_expenses[0]
+        second_saved_expense = saved_expenses[1]
+        self.assertEqual(first_saved_expense.category, 'Salary')
+        self.assertEqual(first_saved_expense.amount, 1000)
+        self.assertEqual(second_saved_expense.category, 'Bonus')
+        self.assertEqual(second_saved_expense.amount, 2000)
