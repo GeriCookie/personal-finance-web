3bcbd7a5d0397b2df81c95c8f5e0860e891f5815
Cookie <gergana.bailova@gmail.com>
2017-06-05 02:24:47 +0300

    Fixed links (removed balance.id)

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,14 +2,17 @@ from django.conf.urls import url, include
 
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import views
 
 urlpatterns = [
-    url('^signup/', CreateView.as_view(
+    url(r'^signup/', CreateView.as_view(
             template_name='signup.html',
             form_class=UserCreationForm,
             success_url='/'
     )),
-    url('^', include('django.contrib.auth.urls')),
+    url(r'^signin/', views.login, {'template_name': 'signin.html'}),
+    url(r'^signout/', views.logout, {'next_page': '/'}),
+
 ]
 
 urlpatterns += [
