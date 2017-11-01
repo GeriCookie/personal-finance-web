f4691999580dc5644af4523bb97f7c28d5e3c6cc
GeriCookie <gergana.bailova@gmail.com>
2017-11-01 09:37:57 +0200

    Changing AWS addres

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -5,11 +5,11 @@ import random
 REPO_URL = 'https://github.com/GeriCookie/personal-finance-web.git'
 env.user = "ubuntu"
 env.key_filename = ["/Users/Cookie/Desktop/COOKIE.pem"]
-env.hosts = ["ec2-52-19-30-160.eu-west-1.compute.amazonaws.com"
+env.hosts = ["ec2-52-215-24-150.eu-west-1.compute.amazonaws.com"
              "ec2-34-251-86-69.eu-west-1.compute.amazonaws.com"]
 
 
-@hosts("ec2-52-19-30-160.eu-west-1.compute.amazonaws.com")
+@hosts("ec2-52-215-24-150.eu-west-1.compute.amazonaws.com")
 def deploy_live():
     site_folder = f'/home/{env.user}/sites/{env.host}'
     source_folder = site_folder + '/source'
